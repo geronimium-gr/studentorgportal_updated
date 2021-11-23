ffe040f2834512506c9054e41c0d63331deaaa1e
geronimium_gr <geronimoadalia@gmail.com>
2021-11-23 15:12:09 +0800

    Approval Events

--- src/app/event-pending/event-pending/event-pending.component.ts
+++ src/app/event-pending/event-pending/event-pending.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
 import { AlertController, ModalController, NavParams } from '@ionic/angular';
 import { Observable } from 'rxjs';
+import { EventzService } from '../../services/eventz.service';
 
 @Component({
   selector: 'app-event-pending',
@@ -18,16 +19,25 @@ export class EventPendingComponent implements OnInit {
   constructor(public modalCtrl: ModalController,
               private afs: AngularFirestore,
               private alertCtrl: AlertController,
-              private navParams: NavParams) 
-              
-  { 
+              private navParams: NavParams,
+              private eventService: EventzService)
+
+  {
     this.eventOrgId = this.navParams.get('orgId');
     console.log(this.eventOrgId);
   }
-  
+
   ngOnInit() {
     this.pendingEvRef = this.afs.collection("eventz", ref => ref.orderBy("createdAt", "desc").where("eventOrgId", "==", this.eventOrgId).where("status", "==", "pending"));
     this.pendingEv = this.pendingEvRef.valueChanges();
+  }
+
+  acceptEvent(eventId) {
+
+  }
+
+  deleteEvent(eventId) {
+
   }
 
   closeModal() {
