f99f5376890ce4314b5b6ef867078e20720c24d9
geronimium_gr <geronimoadalia@gmail.com>
2021-12-08 23:09:53 +0800

    Audit Trail Export

--- src/app/audit-trail/audit-trail/audit-trail.page.ts
+++ src/app/audit-trail/audit-trail/audit-trail.page.ts
@@ -1,9 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { MenuController } from '@ionic/angular';
+import { AlertController, LoadingController, MenuController, ToastController } from '@ionic/angular';
 import { Subscription } from 'rxjs';
 import { AuditTrailService } from '../../services/audit-trail.service';
 import { AuditTrail } from '../../models/audit-trail';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-audit-trail',
@@ -20,6 +24,7 @@ export class AuditTrailPage implements OnInit, OnDestroy {
 
   rows = [];
   loadingRows = [];
+  rowArray = [];
   columns;
 
   isLoading = false;
@@ -27,7 +32,10 @@ export class AuditTrailPage implements OnInit, OnDestroy {
 
   constructor(private menu: MenuController,
               private afs: AngularFirestore,
-              private auditService: AuditTrailService
+              private auditService: AuditTrailService,
+              private alertCtrl: AlertController,
+              private loadingCtrl: LoadingController,
+              private toastCtrl: ToastController
     )
     {
       this.getData();
@@ -39,6 +47,150 @@ export class AuditTrailPage implements OnInit, OnDestroy {
       this.auditList = audits;
       this.isLoading = false;
     });
+  }
+
+  exportCSV() {
+    //Author: Christian Ching
+    this.rowArray.push(["Surname","Firstname", "SchoolID", "Email", "Action", "Timestamp"]);
+
+    this.rows.forEach((u) => {
+      this.rowArray.push([u.userSurname, u.userName, u.userSchoolId, u.userEmail, u.action, new Date(u.createdAt)]);
+    });
+
+    const csvData = this.rowArray.map((r) => r.join(",")).join("\n");
+    const csvContent = "data:text/csv;charset=utf-8," + csvData;
+
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", `audit_${ new Date().getTime() }.csv`);
+    document.body.appendChild(link);
+
+    link.click();
+  }
+
+  async deleteData() {
+    const alert = await this.alertCtrl.create({
+      header: 'Are you absolutely sure?',
+      cssClass: 'deleteAlert',
+      subHeader: 'All records of the Audit Trail will be deleted.',
+      message: `This action cannot be undone. This will permanently delete the
+                records <br> <br>
+                Please type <strong>your password</strong> to confirm.`,
+      inputs: [
+        {
+          name: 'auditText',
+          type: 'password',
+          placeholder: 'Type password here...',
+          attributes: {
+            minLength: 6,
+            inputmode: 'decimal'
+          }
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Delete records',
+          handler: async data => {
+            const loading = await this.loadingCtrl.create({
+              message: 'Wait...',
+              spinner: 'crescent',
+              showBackdrop: true
+            });
+
+            loading.present();
+
+            const currentUser = firebase.auth().currentUser;
+
+            const credentials = firebase.auth.EmailAuthProvider.credential(
+              currentUser.email , data.auditText
+            );
+
+            currentUser.reauthenticateWithCredential(credentials)
+              .then(success => {
+                if (data.auditText.length < 6) {
+                  loading.dismiss();
+                  console.log('Fill Up Password');
+                } else {
+                  loading.dismiss();
+                  console.log('Deleted.');
+                  this.exportCSV();
+                  this.deleteAllData();
+                  this.toast('All records deleted.', 'success');
+                }
+              },
+              error => {
+                console.log(error);
+                if (error.code === "auth/wrong-password") {
+                  loading.dismiss();
+                  this.alertController('Wrong Password', error.message);
+                } else if (error.code === "auth/network-request-failed") {
+                  loading.dismiss();
+                  this.alertController('Network Error', error.message);
+                } else {
+                  loading.dismiss();
+                  this.alertController('Error', error.message);
+                }//if
+              }).catch(error => {
+                loading.dismiss();
+                console.log(error);
+              });
+          }//async data
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  async alertController(header, message){
+    let alert = await this.alertCtrl.create({
+      header: header,
+      message: message,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            this.deleteData();
+          }
+        }
+       ]
+    });
+    alert.present();
+  }//
+
+  async toast(message, status){
+    const toast = await this.toastCtrl.create({
+      message: message,
+      color: status,
+      position: 'top',
+      duration: 2000
+    });
+
+    toast.present();
+  }//
+
+  async deleteAllData() {
+    try {
+      const batch = this.afs.firestore.batch();
+      const userRef = firebase.firestore()
+        .collection('audit')
+        .get();
+        (await userRef).forEach((element) => {
+            batch.delete(element.ref);
+        });
+  
+        return batch.commit();
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 
   getData() {
