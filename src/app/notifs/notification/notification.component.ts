bb89f1c75d44c72541606c91c2ff861f929576a6
geronimium_gr <geronimoadalia@gmail.com>
2021-12-16 23:00:12 +0800

    Notification UI

--- src/app/notifs/notification/notification.component.ts
+++ src/app/notifs/notification/notification.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ModalController, NavParams } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { NotificationsService } from '../../services/notifications.service';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss'],
 })
-export class NotificationComponent implements OnInit {
+export class NotificationComponent implements OnInit, OnDestroy {
 
-  constructor(public modalCtrl: ModalController) { }
+  notifSub: Subscription;
+  notif: any;
+  currentUser: any;
+  orgId: any;
 
-  ngOnInit() {}
+  constructor (public modalCtrl: ModalController,
+               private notifService: NotificationsService,
+               private navParams: NavParams)
+
+  {
+    this.currentUser = this.navParams.get("userId");
+    this.orgId = this.navParams.get("orgId");
+
+    this.notifService.getOrgId(this.orgId, this.currentUser);
+  }
+
+  ngOnInit() {
+    this.notifSub = this.notifService.getNotifications().subscribe(notifs => {
+      this.notif = notifs;
+    });
+  }
 
   closeModal() {
     this.modalCtrl.dismiss();
+  }
+
+  ngOnDestroy() {
+    if (this.notifSub) {
+      this.notifSub.unsubscribe();
+    }
   }
 
 }
--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -1,9 +1,87 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { LoadingController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+interface Notificationz {
+  notifId: string;
+  userId: string;
+  userName: string;
+  userSurname: string;
+  userPhoto: string;
+  userOrgId: string;
+  action: string;
+  notifPhoto: string;
+  createdAt: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { }
+  notifCol: AngularFirestoreCollection<Notificationz>;
+  notifDoc: AngularFirestoreDocument<Notificationz>;
+  notifs: Observable<Notificationz[]>;
+  notif: Observable<Notificationz>;
+
+  orgId = "";
+  currentUserId = "";
+
+  constructor(private afs: AngularFirestore,
+              private loadingCtrl: LoadingController) { }
+
+  getOrgId(orgId, currentUser) {
+    this.orgId = orgId;
+    this.currentUserId = currentUser;
+    this.filterData();
+  }
+
+  filterData() {
+    this.notifCol = this.afs
+    .collection("notification", ref => ref.orderBy("createdAt", "asc"));
+
+    this.notifs = this.notifCol.snapshotChanges().pipe(
+      map(action => {
+        return action.map(a => {
+          const data = a.payload.doc.data() as Notificationz;
+          data.notifId = a.payload.doc.id;
+          return data;
+        })
+      })
+    );
+  }//
+
+  getNotifications() {
+    return this.notifs;
+  }
+
+  async sendNotif(userId, userName, userSurname, userPhoto, orgId, action, notifPhoto) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading...',
+      spinner: 'crescent',
+      showBackdrop: true
+    });
+
+    loading.present();
+
+    const notifId = this.afs.createId();
+
+    this.afs.collection('chat').doc(notifId).set({
+      'notifId': notifId,
+      'userId': userId,
+      'userName': userName,
+      'userSurname': userSurname,
+      'userPhoto': userPhoto,
+      'userOrgId': orgId,
+      'action': action,
+      'notifPhoto': notifPhoto,
+      'createdAt': Date.now()
+    }).then(() => {
+      loading.dismiss();
+    }).catch(error => {
+      loading.dismiss();
+    });
+   }
 }
